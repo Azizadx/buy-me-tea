2c2ca874940d8d15eee234c79a1e692988db81dd
azizadx <nasrallahh55@gmail.com>
2022-10-30 18:14:20 +0300

    different network is added and verify is been written

--- deploy/01-deploy-buy-me-tea.js
+++ deploy/01-deploy-buy-me-tea.js
@@ -1,9 +1,6 @@
 //import 
 //main function
 //calling of main function  
-
-const { network } = require("hardhat")
-
 // function deployFunc() {
 //     console.log("Hi")
 //     hre.getNamedAccounts()
@@ -11,22 +8,36 @@ const { network } = require("hardhat")
 // }
 // module.exports.default = deployFunc
 
+const { network } = require("hardhat")
+const {networkConfig, developmentChains} = require("../hepler-hardhat-config")
+const { verify } = require("../utils/verify")
+
 module.exports = async ({getNamedAccounts, deployments}) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const {chainId} = network.config.chainId
     
     //if chainId is x then use address y
-    
+    let ethUsdPriceFeedAddress
+    if(developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    }
+    else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    //mock contract is minimal version of real world contract that exist on mainnetwork 
+    //to use local testing
+
 
     //well what happens when 
     // when going for localhost or hardhat network we want to use a mock
     const buyMeTea = await deploy("BuyMeTea",{
         from: deployer,
-        args: [/**
-            address
-            //put price feed address
-        */],
+        args: [ethUsdPriceFeedAddress],
         log: true
     })
+    log("-----------------------------------------------------------------")
 }
+
+module.exports.tags = ["all","buymetea"]
